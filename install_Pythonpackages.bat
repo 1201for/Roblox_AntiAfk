a1118caae35885c97ba63741ad8f9cb7e0cdebdf
1201for <69175632+1201for@users.noreply.github.com>
2024-07-24 22:22:25 -0400

    Update install_Pythonpackages.bat

--- install_Pythonpackages.bat
+++ install_Pythonpackages.bat
@@ -1,7 +1,11 @@
 @echo off
+REM Batch script to install required Python packages
+
 echo Installing required Python packages...
+
 pip install pygetwindow
 pip install pyautogui
-pip install keyboard
-echo Installation complete.
+pip install pywin32
+
+echo All packages installed successfully!
 pause
